95cd49650086f567723fd92f28091e880b592150
jfrmm <johnmirra@gmail.com>
2018-01-15 00:21:35 +0000

    Added grid layout to Home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  introImage = {
+    path: '/assets/images/hunter-haley-424256.jpg',
+    color: 'black',
+  };
+
+  tiles = [
+    { text: 'One', cols: 4, rows: 1, color: 'lightblue' },
+    { text: 'Two', cols: 4, rows: 1, color: 'lightgreen' },
+    { text: 'Three', cols: 4, rows: 1, color: 'lightpink' },
+    { text: 'Four', cols: 4, rows: 1, color: '#DDBDF1' },
+  ];
+
   constructor() { }
 
   ngOnInit() {
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 // the Material components we're using
 import { MatButtonModule } from '@angular/material';
 
 @NgModule({
-  imports: [MatButtonModule, MatToolbarModule],
-  exports: [MatButtonModule, MatToolbarModule],
+  imports: [MatButtonModule, MatToolbarModule, MatGridListModule],
+  exports: [MatButtonModule, MatToolbarModule, MatGridListModule],
 })
 export class MaterialModule { }
