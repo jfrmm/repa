d670623a37ab393ee2c9db82bb2936e036842909
Jorge Marcelino <johnmirra@gmail.com>
2018-01-25 23:28:08 +0000

    Update home card layout

--- src/app/_services/mock-areas.ts
+++ src/app/_services/mock-areas.ts
@@ -4,35 +4,55 @@ export const AREAS: Area[] = [
   {
     title: 'Canalização',
     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vehicula, ex sit amet eleifend tempor, libero est commodo nunc, pharetra consectetur ligula massa nec lectus. Nunc a dictum tellus. Praesent vitae vulputate odio. Morbi et felis non justo aliquam suscipit ac ut odio. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed at est dolor.',
-    img: 'jouni-rajala-374132.jpg',
+    // img: 'jouni-rajala-374132.jpg',
+    img: {
+      path: 'jouni-rajala-374132.jpg',
+      position: 'row-reverse',
+    },
     cols: 4,
     rows: 1
   },
   {
     title: 'Electricidade',
     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vehicula, ex sit amet eleifend tempor, libero est commodo nunc, pharetra consectetur ligula massa nec lectus. Nunc a dictum tellus. Praesent vitae vulputate odio. Morbi et felis non justo aliquam suscipit ac ut odio. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed at est dolor.',
-    img: 'neven-krcmarek-424910.jpg',
+    // img: 'neven-krcmarek-424910.jpg',
+    img: {
+      path: 'neven-krcmarek-424910.jpg',
+      position: 'row',
+    },
     cols: 4,
     rows: 1
   },
   {
     title: 'Gás',
     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vehicula, ex sit amet eleifend tempor, libero est commodo nunc, pharetra consectetur ligula massa nec lectus. Nunc a dictum tellus. Praesent vitae vulputate odio. Morbi et felis non justo aliquam suscipit ac ut odio. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed at est dolor.',
-    img: 'chuttersnap-444536.jpg',
+    // img: 'chuttersnap-444536.jpg',
+    img: {
+      path: 'chuttersnap-444536.jpg',
+      position: 'row-reverse',
+    },
     cols: 4,
     rows: 1
   },
   {
     title: 'Estores',
     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vehicula, ex sit amet eleifend tempor, libero est commodo nunc, pharetra consectetur ligula massa nec lectus. Nunc a dictum tellus. Praesent vitae vulputate odio. Morbi et felis non justo aliquam suscipit ac ut odio. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed at est dolor.',
-    img: 'rktkn-109373.jpg',
+    // img: 'rktkn-109373.jpg',
+    img: {
+      path: 'rktkn-109373.jpg',
+      position: 'row',
+    },
     cols: 4,
     rows: 1
   },
   {
     title: 'Electrodomésticos',
     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vehicula, ex sit amet eleifend tempor, libero est commodo nunc, pharetra consectetur ligula massa nec lectus. Nunc a dictum tellus. Praesent vitae vulputate odio. Morbi et felis non justo aliquam suscipit ac ut odio. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed at est dolor.',
-    img: 'dewang-gupta-464449.jpg',
+    // img: 'dewang-gupta-464449.jpg',
+    img: {
+      path: 'dewang-gupta-464449.jpg',
+      position: 'row-reverse',
+    },
     cols: 4,
     rows: 1
   }
