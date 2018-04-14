6bf6d38ad357c56c019b3780907407b7aaa4057f
jfrmm <johnmirra@gmail.com>
2018-04-14 17:15:13 +0100

    Add ngx-carousel

--- src/app/home/scroller/scroller.component.ts
+++ src/app/home/scroller/scroller.component.ts
@@ -23,31 +23,33 @@ export class ScrollerComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    this.carouselTileItems = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+    this.carouselTileItems = [
+      {
+        title: "Vidros partidos?",
+        subTitle: "janelas novas em 24 horas"
+      },
+      {
+        title: "Estores bloqueados?",
+        subTitle: "Reparamos estores em 24 horas"
+      },
+      {
+        title: "Esgotos entupidos?",
+        subTitle: "Orçamentamos em 24 horas"
+      }
+    ];
 
     this.carouselTile = {
-      grid: { xs: 2, sm: 3, md: 3, lg: 5, all: 0 },
-      slide: 2,
-      speed: 400,
+      grid: { xs: 1, sm: 1, md: 1, lg: 1, all: 0 },
+      slide: 1,
+      speed: 250,
+      interval: 3000,
       animation: "lazy",
       point: {
         visible: true
       },
-      load: 2,
       touch: true,
-      easing: "ease"
+      easing: "ease",
+      loop: true
     };
   }
-  public carouselTileLoad(evt: any) {
-    const len = this.carouselTileItems.length;
-    if (len <= 30) {
-      for (let i = len; i < len + 10; i++) {
-        this.carouselTileItems.push(i);
-      }
-    }
-  }
-
-  // carouselLoad will trigger this funnction when your load value reaches
-  // it is helps to load the data by parts to increase the performance of the app
-  // must use feature to all carousel
 }
