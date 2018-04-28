f9e37dae02bfd3c964cce0188c18696a543dead1
jfrmm <johnmirra@gmail.com>
2018-04-28 14:27:08 +0100

    Change Services layout

--- src/app/home/services/services.component.ts
+++ src/app/home/services/services.component.ts
@@ -7,12 +7,67 @@ import { Component, OnInit } from "@angular/core";
 })
 export class ServicesComponent implements OnInit {
   /**
-   * @type array
+   * @type var
    */
-  private _background = {
+  _background = {
     path: "/assets/images/foto_background_servicos.jpg",
     color: "black"
   };
+
+  /**
+   * @type array
+   */
+  _services = [
+    {
+      image: "/assets/icons/icon_canalizacoes.png",
+      title: "Canalizações",
+      p1: "Instalações novas",
+      p2: "Reparações"
+    },
+    {
+      image: "/assets/icons/icon_portas_fechaduras.png",
+      title: "Portas e fechaduras",
+      p1: "Abertura de portas",
+      p2: "Mudança de fechaduras"
+    },
+    {
+      image: "/assets/icons/icon_desentupimentos.png",
+      title: "Desentupimentos",
+      p1: "Lava-loiças",
+      p2: "Sanitas",
+      p3: "Lavatórios e chuveiros",
+      p4: "Pequenos esgotos"
+    },
+    {
+      image: "/assets/icons/icon_electricidade.png",
+      title: "Electricidade",
+      p1: "Instalação de tomadas, interruptores",
+      p2: "Detecção e reparação de curto-circuitos"
+    },
+    {
+      image: "/assets/icons/icon_electrodomesticos.png",
+      title: "Electrodomésticos",
+      p1: "Instalações novas",
+      p2: "Reparação de máquinas de lavar roupa e loiça"
+    },
+    {
+      image: "/assets/icons/icon_caixilharia_estores.png",
+      title: "Caixilharia e estores",
+      p1: "Reparação e substituição de estores",
+      p2: "Mudança e reparação de janelas",
+      p3: "Troca de vidros partidos"
+    },
+    {
+      image: "/assets/icons/icon_gas.png",
+      title: "Gás",
+      p1: "Instalação e reparação de esquentadores e caldeiras"
+    },
+    {
+      image: "/assets/icons/icon_pintura.png",
+      title: "Pintura",
+      p1: "Interiores e exteriores"
+    }
+  ];
 
   constructor() {}
 
