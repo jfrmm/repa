24e71ef209b5888aebafb09dd3d6b89c4b7af981
Jorge Marcelino <jmarcelino@alter-frame.com>
2018-05-04 15:31:18 +0100

    Remove unnecessary Contacts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
-import { ContactsComponent } from "./contacts/contacts.component";
 
 const routes: Routes = [
   {
     path: "",
     component: HomeComponent
-  },
-  {
-    path: "contacts",
-    component: ContactsComponent
   }
 ];
 
