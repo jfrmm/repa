df5bea374d389687fa1a7c612055c693c012ee3b
Jorge Marcelino <jmarcelino@alter-frame.com>
2018-05-08 13:27:34 +0100

    Change carousel content

--- src/app/_services/mock-carousel.ts
+++ src/app/_services/mock-carousel.ts
@@ -5,36 +5,36 @@ export const CAROUSELS: Carousel[] = [
     title: "Vidros partidos?",
     // subTitle: "instalação, reparação e substituição",
     chip: "orçamentos grátis",
-    background: "/assets/images/foto_slider_1.jpg"
+    background: "/assets/images/foto_slider_vidros.jpg"
   },
   {
     title: "Electricidade a falhar?",
     // subTitle: "instalações e reparações",
     chip: "orçamentos grátis",
-    background: "/assets/images/foto_slider_2.jpg"
+    background: "/assets/images/foto_slider_electricidade.jpg"
   },
   {
     title: "Cheiro a gás?",
     // subTitle: "instalações e reparações",
     chip: "orçamentos grátis",
-    background: "/assets/images/foto_slider_3.jpg"
+    background: "/assets/images/foto_slider_gas.jpg"
+  },
+  {
+    title: "Estores partidos?",
+    // subTitle: "",
+    chip: "orçamentos grátis",
+    background: "/assets/images/foto_slider_esgotos.jpg"
   },
   {
     title: "Esgotos entupidos?",
     // subTitle: "instalações, reparações e substituição",
     chip: "orçamentos grátis",
-    background: "/assets/images/foto_slider_4.jpg"
+    background: "/assets/images/foto_slider_esgotos.jpg"
   },
   {
     title: "Problemas com fechaduras?",
     // subTitle: "",
     chip: "orçamentos grátis",
-    background: "/assets/images/foto_slider_5.jpg"
+    background: "/assets/images/foto_slider_fechaduras.jpg"
   },
-  {
-    title: "Torneiras a pingar?",
-    // subTitle: "",
-    chip: "orçamentos grátis",
-    background: "/assets/images/foto_slider_6.jpg"
-  }
 ];
